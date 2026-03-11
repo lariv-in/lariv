908dbef137fef738643d869732858c96f219ceaa
Sandeep Kumar <sandeepqmar05@gmail.com>
2026-03-11 15:27:35 +0530

    hotreloading

--- components/icon.go
+++ components/icon.go
@@ -18,7 +18,7 @@ type Icon struct {
 func (e Icon) Build(ctx context.Context) gomponents.Node {
 	nodes := []gomponents.Node{
 		html.Class(fmt.Sprintf("heroicon %s", e.Classes)),
-		html.StyleAttr(fmt.Sprintf("--heroicon-url: url('https://api.iconify.design/heroicons/%s.svg')", e.Name)),
+		html.Style(fmt.Sprintf("--heroicon-url: url('https://api.iconify.design/heroicons/%s.svg')", e.Name)),
 	}
 	nodes = append(nodes, e.Attrs...)
 	return html.Span(nodes...)
